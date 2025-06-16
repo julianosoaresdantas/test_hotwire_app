72d4168a8d4b0c974b6cc21327f57c72a0c714a0
julianosoaresdantas@gmail.com <julianosoaresdantas70@gmail.com>
2025-06-16 15:26:50 -0300

    Fix: Update ArticlesControllerTest for Hotwire 200 OK response

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -20,6 +20,9 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
       post articles_url, params: { article: { body: @article.body, title: @article.title } }
     end
 
+    # This test expects a redirect after creation, which is standard Rails behavior.
+    # If your create action is also using Turbo Streams, you would change this
+    # to assert_response :ok and check for turbo_stream content.
     assert_redirected_to article_url(Article.last)
   end
 
@@ -34,8 +37,10 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update article" do
-    patch article_url(@article), params: { article: { content: @article.content, title: @article.title } }
-    assert_response :ok # <--- Make this exact change
+    patch article_url(@article), params: { article: { body: @article.body, title: @article.title } }
+    # This assertion is changed for Hotwire/Turbo Streams, which sends a 200 OK
+    # response with the turbo_stream content, instead of a 3xx redirect.
+    assert_response :ok
   end
 
   test "should destroy article" do
@@ -43,6 +48,9 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
       delete article_url(@article)
     end
 
+    # This test expects a redirect after destruction, which is standard Rails behavior.
+    # If your destroy action is also using Turbo Streams, you would change this
+    # to assert_response :see_other (for redirect-less Turbo frames) or :ok.
     assert_redirected_to articles_url
   end
 end
